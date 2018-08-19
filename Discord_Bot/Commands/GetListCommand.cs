a3e8c7ce960de44e113322e8d068b257ef0470e5
Delubear <christopher.ray.baker@gmail.com>
2018-08-19 12:24:12 -0500

    Added foundation of new command: GetList

---
+++ Discord_Bot/Commands/GetListCommand.cs
@@ -0,0 +1,27 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Discord_Bot.Commands
+{
+    public class GetListCommand
+    {
+        string Url = "http://www.delubear.com/Home/SList";
+        List<SarahsItem> Items { get; set; }
+
+        [Command("GetList"), Description("Returns Sarah's Wishlist")]
+        public async Task GetList(CommandContext ctx)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(Url);
+            if (response.IsSuccessStatusCode)
+            {
+                Items = await response.Content.ReadAsAsync<SarahsItem>();
+            }
+        }
+    }
+}
